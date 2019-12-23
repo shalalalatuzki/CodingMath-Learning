37fce064a585ce34f9d51d0b1fa90f1633bfdd88
your name <your email>
2019-12-23 14:14:17 +0800

    2.5Dpractice

--- six/stars.js
+++ six/stars.js
@@ -5,50 +5,56 @@ window.onload = function() {
         height = canvas.height = window.innerHeight,
         fl = 300,
         cards = [],
-        numCards = 7,
-        centerZ = 1000,
+        numcards = 20,
+        CenterZ = 1000,
         radius = 1000,
+        ypos = 0,
         baseAngle = 0,
-        rotationSpeed = 0.01;
-    for (var i = 0; i < numCards; i++) {
+        rotationSpeed = 0.01,
+        star = document.createElement("img");
+
+    star.src = "star.png";
+
+    for (var i = 0; i < numcards; i += 1) {
         var card = {
-            y: 0,
-            angle: Math.PI * 2 / numCards * i,
+            angle: Math.PI * 2 / numcards * i,
             img: document.createElement("img")
         };
-        card.img.src = "postcard" + i + ".jpg";
         card.x = Math.cos(card.angle + baseAngle) * radius;
-        card.z = centerZ + Math.sin(card.angle + baseAngle) * radius;
+        card.z = CenterZ + Math.sin(card.angle + baseAngle) * radius;
         cards.push(card);
     }
 
     context.translate(width / 2, height / 2);
-    context.font = "200px Arial";
+    context.font = "200 Arial";
 
     document.body.addEventListener("mousemove", function(event) {
         rotationSpeed = (event.clientX - width / 2) * 0.00005;
+        ypos = (event.clientY - height / 2) * 2;
     });
+
     update();
 
     function update() {
         baseAngle += rotationSpeed;
         cards.sort(zsort);
         context.clearRect(-width / 2, -height / 2, width, height);
-        for (var i = 0; i < numCards; i++) {
+
+        for (var i = 0; i < numcards; i += 1) {
             var card = cards[i],
                 perspective = fl / (fl + card.z);
 
             context.save();
             context.scale(perspective, perspective);
-            context.translate(card.x, card.y);
+            context.translate(card.x, ypos);
 
             context.translate(-card.img.width / 2, -card.img.height / 2);
-            context.drawImage(card.img, 0, 0);
+            context.drawImage(star, 0, 0);
 
             context.restore();
 
             card.x = Math.cos(card.angle + baseAngle) * radius;
-            card.z = centerZ + Math.sin(card.angle + baseAngle) * radius;
+            card.z = CenterZ + Math.sin(card.angle + baseAngle) * radius;
         }
         requestAnimationFrame(update);
     }
@@ -56,5 +62,4 @@ window.onload = function() {
     function zsort(cardA, cardB) {
         return cardB.z - cardA.z;
     }
-
 }
